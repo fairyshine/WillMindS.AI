83ad9018e34013162a9a70dd05a88dfbbf76a4fc
Radicat <2285225334@qq.com>
2025-03-07 22:12:13 +0800

    🔨[DEV] try grpo

--- tests/minimind_train.py
+++ tests/minimind_train.py
@@ -14,29 +14,50 @@ from willminds.pipeline.MiniMind_trainer import compute_loss_func
 from willminds.pipeline.MiniMind_trainer import Trainer as MiniMind_Trainer
 
 
-tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_path)
-# * Pretrain
-# model = MiniMindLM(MiniMindConfig(**config.model)).to(config.train.device)
-# train_dataset = PretrainDataset(config.train.train_data_path, tokenizer, max_length=config.train.max_seq_len)
-# * SFT
-model = MiniMindLM.from_pretrained(config.checkpoint)
-train_dataset = SFTDataset(config.train.train_data_path, tokenizer, max_length=config.train.max_seq_len)
+# tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_path)
+# # * Pretrain
+# # model = MiniMindLM(MiniMindConfig(**config.model)).to(config.train.device)
+# # train_dataset = PretrainDataset(config.train.train_data_path, tokenizer, max_length=config.train.max_seq_len)
+# # * SFT
+# model = MiniMindLM.from_pretrained(config.checkpoint)
+# train_dataset = SFTDataset(config.train.train_data_path, tokenizer, max_length=config.train.max_seq_len)
 
-logger.info(f'LLM总参数量：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f} 百万')
+# logger.info(f'LLM总参数量：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f} 百万')
 
-optimizer = optim.AdamW(model.parameters(), lr=config.train.learning_rate)
-scheduler = optim.lr_scheduler.LambdaLR(
-    optimizer=optimizer, 
-    lr_lambda=lambda step : 0.1 + 0.5 * (1 + math.cos(math.pi*step/(config.train.num_train_epochs*len(train_dataset)/config.train.per_device_train_batch_size/config.train.gradient_accumulation_steps)))
+# optimizer = optim.AdamW(model.parameters(), lr=config.train.learning_rate)
+# scheduler = optim.lr_scheduler.LambdaLR(
+#     optimizer=optimizer, 
+#     lr_lambda=lambda step : 0.1 + 0.5 * (1 + math.cos(math.pi*step/(config.train.num_train_epochs*len(train_dataset)/config.train.per_device_train_batch_size/config.train.gradient_accumulation_steps)))
+# )
+
+# trainer = Trainer(model=model, 
+#                   args=monitor.trainer_args,
+#                   train_dataset=train_dataset,
+#                   compute_loss_func=compute_loss_func,
+#                   callbacks=[monitor.tracking_callback],
+#                   optimizers=(optimizer,scheduler))
+# trainer.train()
+
+# ---- RL ---------
+from datasets import load_dataset
+from trl import GRPOConfig, GRPOTrainer
+
+dataset = load_dataset("trl-lib/tldr", split="train")
+
+# Define the reward function, which rewards completions that are close to 20 characters
+def reward_len(completions, **kwargs):
+    return [-abs(20 - len(completion)) for completion in completions]
+
+training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", logging_steps=10)
+trainer = GRPOTrainer(
+    # model="Qwen/Qwen2-0.5B-Instruct",
+    model = MiniMindLM.from_pretrained(config.checkpoint),
+    reward_funcs=reward_len,
+    args=training_args,
+    train_dataset=dataset,
 )
-
-trainer = Trainer(model=model, 
-                  args=monitor.trainer_args,
-                  train_dataset=train_dataset,
-                  compute_loss_func=compute_loss_func,
-                  callbacks=[monitor.tracking_callback],
-                  optimizers=(optimizer,scheduler))
 trainer.train()
+
 
 # # -- pretrain (RAW) --
 
