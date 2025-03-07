10116c249ccf93ac8d914526198e0e56caadd743
Radicat <2285225334@qq.com>
2025-03-07 14:21:11 +0800

    ⚠️[FIX] minor fix

--- src/willminds/monitor/__init__.py
+++ src/willminds/monitor/__init__.py
@@ -1,5 +1,8 @@
 import datetime
 
+from dataclasses import fields
+from transformers import TrainingArguments
+
 from .config import (
     get_config, 
     set_seed, 
@@ -25,6 +28,8 @@ class Monitor:
         set_seed(self.config.train.seed)
 
         log_print_config(self.config, self.logger)
+
+        self.trainer_args = TrainingArguments(TrainingArguments(**{k: v for k, v in self.config.train.items() if k in {f.name for f in fields(TrainingArguments)}}))
     
 
 
--- tests/minimind_train.py
+++ tests/minimind_train.py
@@ -5,7 +5,7 @@ from torch import optim
 from torch.utils.data import DataLoader
 
 from transformers import AutoTokenizer
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer
 
 from willminds import monitor, config, logger
 from willminds.data.corpus.MiniMind_dataset import PretrainDataset, SFTDataset
@@ -22,31 +22,31 @@ model = MiniMindLM(LMConfig(**config.model))
 train_dataset = PretrainDataset(config.train.train_data_path, tokenizer, max_length=config.train.max_seq_len)
 logger.info(f'LLM总参数量：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f} 百万')
 
-train_loader = DataLoader(
-            train_dataset,
-            batch_size=config.train.per_device_train_batch_size,
-            pin_memory=True,
-            drop_last=False,
-            shuffle=False,
-            num_workers=config.train.dataloader_num_workers,
-            sampler=None
-        )
+# train_loader = DataLoader(
+#             train_dataset,
+#             batch_size=config.train.per_device_train_batch_size,
+#             pin_memory=True,
+#             drop_last=False,
+#             shuffle=False,
+#             num_workers=config.train.dataloader_num_workers,
+#             sampler=None
+#         )
 
-trainer = MiniMind_Trainer(model.to(config.train.device), train_loader)
-trainer.train()
-
-# optimizer = optim.AdamW(model.parameters(), lr=config.train.learning_rate)
-# scheduler = optim.lr_scheduler.LambdaLR(
-#     optimizer=optimizer, 
-#     lr_lambda=lambda epoch : 0.1 + 0.5 * (1 + math.cos(math.pi * epoch / (config.train.num_train_epochs*len(train_dataset)/config.train.per_device_train_batch_size))))
-
-# trainer = Trainer(model=model, 
-#                   args=TrainingArguments(**config.train),
-#                   train_dataset=train_dataset,
-#                   compute_loss_func=compute_loss_func,
-#                   callbacks=[monitor.tracking_callback],
-#                   optimizers=(optimizer,scheduler))
+# trainer = MiniMind_Trainer(model.to(config.train.device), train_loader)
 # trainer.train()
+
+optimizer = optim.AdamW(model.parameters(), lr=config.train.learning_rate)
+scheduler = optim.lr_scheduler.LambdaLR(
+    optimizer=optimizer, 
+    lr_lambda=lambda epoch : 0.1 + 0.5 * (1 + math.cos(math.pi * epoch / (config.train.num_train_epochs*len(train_dataset)/config.train.per_device_train_batch_size))))
+
+trainer = Trainer(model=model, 
+                  args=monitor.trainer_args,
+                  train_dataset=train_dataset,
+                  compute_loss_func=compute_loss_func,
+                  callbacks=[monitor.tracking_callback],
+                  optimizers=(optimizer,scheduler))
+trainer.train()
 
 # -- SFT --
 
